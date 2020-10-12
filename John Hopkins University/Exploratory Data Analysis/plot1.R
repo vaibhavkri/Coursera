12e9acd5dbf7b3722f3ec47e17eb7b709ae5ad6a
vaibhavkri <vaibhavkrishna.bhosle@gmail.com>
2020-10-12 18:30:18 +0530

    update

--- John Hopkins University/Exploratory Data Analysis/plot1.R
+++
@@ -1,4 +0,0 @@
-#using mydata1 which is filtered 2 day data see main.R for code
-hist(as.numeric(mydata1$Global_active_power),breaks=6,col="red",main="Global Active Power", xlab="Global Active Power(kilowatts")
-dev.copy(png,"plot1.png")
-dev.off()
